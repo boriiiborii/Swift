b6572d99be113905794c679208d145f1c5afb422
leehwajin <ghkwlsspdl@naver.com>
2022-08-18 23:42:00 +0900

    15.3

--- 15.2_protocol/15.2_protocol/Document.swift
+++ 15.2_protocol/15.2_protocol/Document.swift
@@ -1,74 +1,186 @@
 import UIKit
 
+
+//메서드의 요구사항 정의하는 방법
+//프로토콜메서드요구사항
+//메서드의 헤드부분의 형태만 요구사항으로 정의
+//mutating키워드: 구조체로제한하는거아님. 구조체에서 저장속성을 변경하는 경우, 구조체도 채택 가능하도록 허락하는 키워드
+//타입메서드로 정의하려면 static키워드만 붙이면 됨
+//채택해서 구현하는 쪽에서 static class키워드 모두 사용
+
 //1)정의
-protocol MyProtocol {
-    //최소한의 요구사항 나열
+protocol RandomNumber {
+    static func reset()
+    func random() -> Int
+    //mutating func doSomthing()
 }
 
-class FamilyClass {}
-
-//2)채택(클래스, 구조체, 열거형 다 가능)
-class MyClass: FamilyClass, MyProtocol {
-    //3)(속성/메서드) 구체적인 구현
-}
-
-//프로토콜을 채택하려는 클래스, 구조체, 열거형에 최소한 이런 내용을 구현해야한다고 선언하는 부분
-//1. 속성요구사항
-//2. 메서드의 요구사항(메서드/생성자/서브스크립트)
-
-//프로토콜속성의 요구사항
-//속성의 뜻에서 var로 선언(let으로 선언불가)
-//get, set키워드를 통해서 읽기/쓰기 여부를 설정(최소한의 요구사항일뿐)
-//저장속성/계산속성 모두 구현 가능
-
-protocol RemoteMouse {
+//2)채택 및 구현
+class Number: RandomNumber {
     
-    var id:String {get} //let저장속성/ var저장속성/ 읽기계산속성/ 읽기쓰기 계산속성 가능
-    
-    var name:String {get set} // var저장속성/ 읽기,쓰기계산속성
-    
-    static var type: String {get set} // 타입계산속성(static, class)
-    
-}
-
-//채택하면, 최소한의 요구사항을 정확하게 따라서 구현해야함
-//인스턴스 저장/계산속성-----------
-struct TV: RemoteMouse {
-    var id: String = "456"
-    
-    var name: String = "애플티비"
-    
-    static var type:String = "리모콘"
-}
-
-let myTV = TV()
-myTV.id
-myTV.name
-TV.type
-
-//타입속성 ===========
-//1)저장타입속성으로 구현
-class SmartPhone: RemoteMouse {
-    var id: String {
-        return "777"
+    static func reset() {
+        print("다시 세팅")
     }
     
-    var name: String {
-        get {"아이폰"}
-        set{ }
+    func random() -> Int {
+        return Int.random(in:1...100)
     }
-    
-    static var type: String = "리모콘"  //타입 저장 속성은 상속은 되지만 재정의 원칙적 불가능
 }
 
-//2)계산 타입 속성으로 구현
-class Ipad: RemoteMouse {
-    var id: String = "777"
+let n = Number()
+n.random()
+Number.reset()
+
+//1)정의
+protocol Togglable {
+    mutating func toggle()
+}
+
+//2)채택 및 구현
+enum OnOffSwitch: Togglable {
+    case on, off
     
-    var name: String = "아이패드"
-    
-    class var type: String { //타입계산속성은 재정의 가능(class키워드 가능)
-        get {"리모콘"}
-        set {  }
+    mutating func toggle() {
+        switch self {
+        case .off:
+            self = .on
+        case .on:
+            self = .off
+        }
     }
 }
+
+var s = OnOffSwitch.off
+s.toggle()
+s.toggle()
+
+class BigSwitch: Togglable {
+    var isOn = false
+    func toggle() {
+        isOn = isOn ? false : true
+    }
+}
+
+var big = BigSwitch()
+print(big.isOn)
+big.toggle()
+print(big.isOn)
+
+
+
+
+
+//메서드 요구사항 = 생성자 요구사항
+
+protocol Someprotocol {
+    init(num: Int)
+}
+
+//생서자요구사항
+//클래스는 생성자 앞에 required를 붙여야함(하위구현강제) (구조체의 경우 상속이 없기 때문에 , requried키워드 필요없음)
+//아니면 final을 붙여서 상속을 막으면 required생략가능
+//클래스에서는 반드시 지정생성자로 구현할 필요는 없음(편의생성자로 구현도 가능)
+
+//예제1
+class SomeClass: Someprotocol {
+    required init(num: Int){ //실제 구현
+        
+    }
+}
+
+class SomeSubClass: SomeClass {
+    //하위클래스에서 생성자 구현을 안하면 필수 생성자는 자동 상속
+    //requried init(num: Int)
+}
+
+//예제2
+protocol AProtocol {
+    init()
+}
+
+class ASuperClass {
+    init() {
+        //생성자의 내용 구현
+    }
+}
+
+class BSubClass: ASuperClass, AProtocol {
+    //AProtocol을 채택함으로 "reuired"키워드가 필요하고, 상속으로 인한 override(재정의)" 재정의 키워드도 필요
+    required override init() {
+        
+    }
+}
+
+
+
+
+//------------------------------------
+//생성자의 요구사항 - 실패가능 생성자의 경우
+//init?() >> init()/init?()/init!()로구현가능
+//init() >> init?()로 구현 불가능. 범위가 더 넓어지는것 불가
+
+//실패가능 생성자
+protocol Aproto {
+    init?(num: Int)
+}
+
+struct AStrutct: Aproto {
+    //init?(num: Int) {{
+    init(num: Int) {}
+    //init!(num: Int) {} //이거도 괜찮음
+}
+
+//클래스에서 채택
+class AClass: Aproto{
+    required init(num: Int) {}
+}
+
+
+
+//-----------------------------------------
+//메서드 요구사항 - 서브스크립트 요구사항
+//get set 키워드 통해서 읽기/쓰기 여부를 설정(최소한의 요구사항일뿐)
+//get키워드 >> 최소한의 읽기 서브스크립트구현/읽기,쓰기 모두 구현가능
+//get 키워드 >> 반드시 읽기, 쓰기 모두 구현해야함
+
+protocol DataList {
+    subscript(idx: Int) -> Int{get} //(서브스크립트 문법에서) get 필수 (set선택)
+}
+
+struct DataStructure: DataList {
+    //subscript(idx: Int) -> Int {//읽기전용 서브스크립트로 구현한다면
+    //  get{
+    //      return 0
+    //  }
+    //}
+    subscript(idx: Int) -> Int {
+        get {
+            return 0
+        }
+        set {
+            //상세구현
+        }
+    }
+}
+
+
+//---------------------------------------------
+//관습적인 프로토콜 채택과 구현 - 확장에서
+protocol Certificate {
+    func doSomething()
+}
+
+class Person {
+    
+}
+
+//관습적으로 본체보다는 확장에서, 채택구현(코드의 깔끔한 정리 가능)
+extension Person: Certificate{
+    func doSomething() {
+        print("Do Something")
+    }
+}
+
+var a = Person()
+a.doSomething()
+
